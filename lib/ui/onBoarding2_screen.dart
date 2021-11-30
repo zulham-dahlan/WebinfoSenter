e9b7dbc1f9c30809b79e736b88564b75b9ef12be
fianrmdni <fianrmdni@gmail.com>
2021-11-30 22:34:27 +0700

    Menambahkan halaman onBoarding kedua dan melakukan perpindahan menggunakan MaterialPageRoute

--- lib/ui/onBoarding2_screen.dart
+++ lib/ui/onBoarding2_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:webinfo_senter/common/color_palette.dart';
-import 'package:webinfo_senter/ui/onBoarding2_screen.dart';
 
-class OnBoarding extends StatelessWidget {
-  OnBoarding({Key? key}) : super(key: key);
+class OnBoarding2 extends StatelessWidget {
+  OnBoarding2({Key? key}) : super(key: key);
 
   var styleRoboto = const TextStyle(
     fontSize: 18,
@@ -40,38 +39,34 @@ class OnBoarding extends StatelessWidget {
                   padding: const EdgeInsets.all(20),
                 ),
                 Text(
-                  "Webinar dari rumah\nhadir online bareng family",
+                  "Tentuin jadwal senggang\nbuat join dari rumah",
                   style: styleRoboto,
                   textAlign: TextAlign.center,
                 ),
                 SizedBox(height: 6),
                 Text(
-                  "Ikut webinar makin gampang\npake Webinfo Senter",
+                  "Makin produktif sama waktu fleksibel\nasikin di rumah pake Webinfo Senter",
                   style: styleMontserrat,
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 48),
+                SizedBox(height: 45),
                 Container(
-                  margin: EdgeInsets.only(right: 60,left: 60,bottom: 70),
+                  margin: EdgeInsets.only(right: 50,left: 50,bottom: 68),
                   child: ElevatedButton(
                     onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => OnBoarding2()),
-                      );
                     },
                     child: Text("Gass!!"),
                     style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25.0),
-                                side: BorderSide(color: Colors.red)
-                            )
-                        ),
-                        backgroundColor: MaterialStateProperty.all(ColorPalette.customRedColor),
-                        padding: MaterialStateProperty.all(EdgeInsets.only(right: 101,left: 101,top: 16,bottom: 16)),
-                        textStyle: MaterialStateProperty.all(stylePoppins),),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              side: BorderSide(color: Colors.red))),
+                      backgroundColor:
+                          MaterialStateProperty.all(ColorPalette.customRedColor),
+                      padding: MaterialStateProperty.all(EdgeInsets.only(
+                          right: 111, left: 111, top: 19, bottom: 18)),
+                      textStyle: MaterialStateProperty.all(stylePoppins),
+                    ),
                   ),
                 ),
               ],
