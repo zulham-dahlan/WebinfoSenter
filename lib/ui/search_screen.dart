d1601745cea9ab60aa3c03e71fce930e51c84bde
zulham321 <zulhamlanz@gmail.com>
2021-12-02 23:46:48 +0700

    Menambahkan Search Screen

---
+++ lib/ui/search_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+
+  static const routeName = '/search_screen';
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
