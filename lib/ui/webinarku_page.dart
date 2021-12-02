a3834d11f6909058763026ccfdf4ce3d429f8a14
zulham321 <zulhamlanz@gmail.com>
2021-12-02 23:36:32 +0700

    Membuat Tampilan Page Webinarku

--- lib/ui/webinarku_page.dart
+++ lib/ui/webinarku_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webinfo_senter/common/style.dart';
 
 class WebinarkuPage extends StatelessWidget {
   static const String webinarkuTitle = 'Webinarku';
@@ -7,8 +8,70 @@ class WebinarkuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Webinarku Page'),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Webinarku',
+                style: styleRoboto,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('On Going'),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context,int index){
+                    return Card(child: Center(child: Text('Webinar $index')));
+                  }
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Finish'),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context,int index){
+                    return Card(child: Center(child: Text('Webinar $index')));
+                  }
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Your Publish'),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context,int index){
+                    return Card(child: Center(child: Text('Webinar $index')));
+                  }
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
