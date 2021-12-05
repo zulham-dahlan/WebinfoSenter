3352267439e45f8dd89038934212d6e641b46aa5
zulham321 <zulhamlanz@gmail.com>
2021-12-05 08:46:28 +0700

    Membuat Class User dan Webinar

---
+++ lib/data/user.dart
@@ -0,0 +1,13 @@
+class User{
+  String nama ;
+  String email ;
+  String password ;
+  String urlFotoProfil ;
+
+  User({
+    required this.nama,
+    required this.email,
+    required this.password,
+    required this.urlFotoProfil,
+  });
+}
---
+++ lib/data/webinar.dart
@@ -0,0 +1,31 @@
+class Webinar{
+  String judul ;
+  String penyelenggara ;
+  String deskripsi ;
+  String tglBukaDaftar ;
+  String tglTutupDaftar ;
+  String tglAcara ;
+  String waktuAcara ;
+  String lokasiAcara ;
+  String harga ;
+  String linkPendaftaran ;
+  String contactPerson ;
+  String sosmed ;
+  String benefit ;
+
+  Webinar({
+    required this.judul,
+    required this.penyelenggara,
+    required this.deskripsi,
+    required this.tglBukaDaftar,
+    required this.tglTutupDaftar,
+    required this.tglAcara,
+    required this.waktuAcara,
+    required this.lokasiAcara,
+    required this.harga,
+    required this.linkPendaftaran,
+    required this.contactPerson,
+    required this.sosmed,
+    required this.benefit,
+  });
+}
