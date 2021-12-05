5c97c237f648fe1c66493b072fc30b076b5fb506
zulham321 <zulhamlanz@gmail.com>
2021-12-05 17:14:25 +0700

    Modifikasi Card Webinar

--- lib/widget/card_webinar_vertical.dart
+++ lib/widget/card_webinar_vertical.dart
@@ -2,43 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:webinfo_senter/data/webinar.dart';
 
 class CardWebinarVertical extends StatelessWidget {
-  final Webinar webinar ;
+  final Webinar webinar;
 
   const CardWebinarVertical({required this.webinar});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: Image.asset(webinar.urlPoster, fit: BoxFit.cover,scale: 0.5, width: 65, height: 85,)
-          ),
-          SizedBox(width: 10,),
-          Column(
-            children: [
-              Text(
-                webinar.judul,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              Text(
-                webinar.penyelenggara,
-              ),
-              Row(
-                children: [
-                  Icon(Icons.schedule),
-                  Text(webinar.tglAcara),
-                  Text(' - ${webinar.waktuAcara}'),
-                ],
-              )
-            ],
-          )
-
-        ],
-      )
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.all(5),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(5.0),
+                child: Image.asset(
+                  webinar.urlPoster,
+                  fit: BoxFit.cover,
+                  width: 65,
+                  height: 85,
+                )),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  webinar.judul,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  webinar.penyelenggara,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_today,
+                      size: 12,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text('${webinar.tglAcara} - ${webinar.waktuAcara}'),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
