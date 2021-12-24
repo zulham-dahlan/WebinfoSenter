836b9cb6ad71c40e1f4fbb81e024fef39546a6f7
fianrmdni <fianrmdni@gmail.com>
2021-12-24 22:46:22 +0700

    Menambahkan fitur add image ke halaman pengajuan

--- lib/ui/pengajuan_screen.dart
+++ lib/ui/pengajuan_screen.dart
@@ -3,6 +3,16 @@ import 'package:webinfo_senter/common/style.dart';
 import 'package:webinfo_senter/data/firebase/firestore_service.dart';
 import 'package:webinfo_senter/data/model/webinar.dart';
 
+
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:path/path.dart' as Path;
+
+
 class FormPengajuan extends StatefulWidget {
   static const routeName = '/form_pengajuan';
 
@@ -28,17 +38,83 @@ class _FormPengajuanState extends State<FormPengajuan> {
   String dropDownValue = 'Pengembangan Diri';
   late String urlPoster;
 
+  bool uploading = false;
+  double val = 0;
+  late CollectionReference imgRef;
+  late firebase_storage.Reference ref;
+
+  final List<File> _image = [];
+  final picker = ImagePicker();
   @override
+
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        appBar: AppBar(
+          title: const Text('Add Image'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    uploading = true;
+                  });
+                  uploadFile().whenComplete(() => Navigator.of(context).pop());
+                },
+                child: const Text(
+                  'upload',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4),
+              child: GridView.builder(
+                  itemCount: _image.length + 1,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (context, index) {
+                    return index == 0
+                        ? Center(
+                      child: IconButton(
+                          icon: const Icon(Icons.add),
+                          onPressed: () =>
+                          !uploading ? chooseImage() : null),
+                    )
+                        : Container(
+                      margin: const EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: FileImage(_image[index - 1]),
+                              fit: BoxFit.cover)),
+                    );
+                  }),
+            ),
+            uploading
+                ? Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'uploading...',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    CircularProgressIndicator(
+                      value: val,
+                      valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
+                    )
+                  ],
+                ))
+                : Container(),
+
+            Column(
               children: [
                 Container(
+                  margin: EdgeInsets.symmetric(vertical: 50),
                   alignment: Alignment.center,
                   child: Text(
                     'Ajukan Webinar',
@@ -273,10 +349,55 @@ class _FormPengajuanState extends State<FormPengajuan> {
                   ],
                 )
               ],
-            ),
-          ),
-        ),
-      ),
-    );
+            )
+          ],
+        ));
+  }
+
+  chooseImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image.add(File(pickedFile!.path));
+    });
+    if (pickedFile!.path == null) retrieveLostData();
+  }
+
+  Future<void> retrieveLostData() async {
+    final LostDataResponse response = await picker.retrieveLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+      setState(() {
+        _image.add(File(response.file!.path));
+      });
+    } else {
+      print(response.file);
+    }
+  }
+
+  Future uploadFile() async {
+    int i = 1;
+
+    for (var img in _image) {
+      setState(() {
+        val = i / _image.length;
+      });
+      ref = firebase_storage.FirebaseStorage.instance
+          .ref()
+          .child('images/${Path.basename(img.path)}');
+      await ref.putFile(img).whenComplete(() async {
+        await ref.getDownloadURL().then((value) {
+          imgRef.add({'url': value});
+          i++;
+        });
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    imgRef = FirebaseFirestore.instance.collection('imageURLs');
   }
 }
