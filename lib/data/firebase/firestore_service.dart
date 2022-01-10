bdb5d1dc37091fd073d1f8eac306d7d174d2d058
zulham321 <zulhamlanz@gmail.com>
2022-01-10 08:02:11 +0700

    memperbaiki akun provider

--- lib/data/firebase/firestore_service.dart
+++ lib/data/firebase/firestore_service.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:webinfo_senter/data/firebase/auth_service.dart';
 import 'package:webinfo_senter/data/model/akun.dart';
@@ -10,68 +12,26 @@ class FirestoreService {
       FirebaseFirestore.instance.collection('webinar');
 
   static Future<void> addAkun(Akun akun, String documentId) async {
-    await akunCollection.doc(documentId).set({
-      'nama': akun.nama,
-      'email': akun.email,
-      'url_foto_profil': akun.urlFotoProfil,
-      'bookmark': akun.bookmark,
-      'publish': akun.publish
-    });
+    await akunCollection.doc(documentId).set(akun.toJson());
   }
 
-  static Future<Akun> readData() async {
+  static Future<Akun> readDataAkun() async {
     String documentId = await AuthServices.getId();
     DocumentSnapshot snapshot = await akunCollection.doc(documentId).get();
     Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-    Akun akun = Akun(
-        nama: data['nama'],
-        email: data['email'],
-        urlFotoProfil: data['url_foto_profil'],
-        bookmark: [],
-        publish: []);
-    return akun;
+    return Akun.fromJson(data);
   }
 
   static Future<void> addWebinar(Webinar webinar) async{
-    await webinarCollection.doc().set({
-        'judul': webinar.judul,
-        'penyelenggara': webinar.penyelenggara,
-        'deskripsi': webinar.deskripsi,
-        'tgl_acara': webinar.tglAcara,
-        'waktu_acara': webinar.waktuAcara,
-        'lokasi_acara': webinar.lokasiAcara,
-        'harga': webinar.harga,
-        'link_pendaftaran': webinar.linkPendaftaran,
-        'contact_person': webinar.contactPerson,
-        'sosmed': webinar.sosmed,
-        'benefit': webinar.benefit,
-        'url_poster': webinar.urlPoster,
-        'kategori': webinar.kategori,
-    });
+    await webinarCollection.doc().set(webinar.toJson());
   }
   
   static Future<List<Webinar>> getDataWebinar() async {
     final List<Webinar> dataWebinar = [];
     await webinarCollection.get().then((snapshot) {
-      snapshot.docs.forEach((element) {
-        Map<String, dynamic> data = element.data() as Map<String, dynamic>;
-        Webinar webinar = Webinar(
-          judul: data['judul'],
-          penyelenggara: data['penyelenggara'],
-          deskripsi: data['deskripsi'],
-          tglAcara: data['tgl_acara'],
-          waktuAcara: data['waktu_acara'],
-          lokasiAcara: data['lokasi_acara'],
-          harga: data['harga'],
-          linkPendaftaran: data['link_pendaftaran'],
-          contactPerson: data['contact_person'],
-          sosmed: data['sosmed'],
-          benefit: data['benefit'],
-          urlPoster: data['url_poster'],
-          kategori: data['kategori'],
-        );
-        dataWebinar.add(webinar);
-      });
+      for (var element in snapshot.docs) {
+        dataWebinar.add(Webinar.fromJson(element.data() as Map<String,dynamic>));
+      }
     });
     return dataWebinar;
   }
@@ -82,25 +42,9 @@ class FirestoreService {
         .where('kategori', isEqualTo: kategori)
         .get()
         .then((snapshot) {
-      snapshot.docs.forEach((element) {
-        Map<String, dynamic> data = element.data() as Map<String, dynamic>;
-        Webinar webinar = Webinar(
-          judul: data['judul'],
-          penyelenggara: data['penyelenggara'],
-          deskripsi: data['deskripsi'],
-          tglAcara: data['tgl_acara'],
-          waktuAcara: data['waktu_acara'],
-          lokasiAcara: data['lokasi_acara'],
-          harga: data['harga'],
-          linkPendaftaran: data['link_pendaftaran'],
-          contactPerson: data['contact_person'],
-          sosmed: data['sosmed'],
-          benefit: data['benefit'],
-          urlPoster: data['url_poster'],
-          kategori: data['kategori'],
-        );
-        dataWebinar.add(webinar);
-      });
+      for (var element in snapshot.docs) {
+        dataWebinar.add(Webinar.fromJson(element.data() as Map<String,dynamic>));
+      }
     });
     return dataWebinar;
   }
@@ -112,35 +56,22 @@ class FirestoreService {
         .where('judul', isLessThan: query + 'z')
         .get()
         .then((snapshot) {
-      snapshot.docs.forEach((element) {
-        Map<String, dynamic> data = element.data() as Map<String, dynamic>;
-        Webinar webinar = Webinar(
-          judul: data['judul'],
-          penyelenggara: data['penyelenggara'],
-          deskripsi: data['deskripsi'],
-          tglAcara: data['tgl_acara'],
-          waktuAcara: data['waktu_acara'],
-          lokasiAcara: data['lokasi_acara'],
-          harga: data['harga'],
-          linkPendaftaran: data['link_pendaftaran'],
-          contactPerson: data['contact_person'],
-          sosmed: data['sosmed'],
-          benefit: data['benefit'],
-          urlPoster: data['url_poster'],
-          kategori: data['kategori'],
-        );
-        dataWebinar.add(webinar);
-      });
+      for (var element in snapshot.docs) {
+        dataWebinar.add(Webinar.fromJson(element.data() as Map<String, dynamic>));
+      }
     });
     return dataWebinar;
   }
 
   static Future<void> addBookmark(List<Webinar> listBookmark) async {
     String documentId = await AuthServices.getId();
-    var listMapBookmark = await convertToMap(listBookmark);
+    final List<Map<String,dynamic>> list = [];
+    for(var element in listBookmark){
+      list.add(element.toJson());
+    }
     await akunCollection
         .doc(documentId)
-        .update({'bookmark': FieldValue.arrayUnion(listMapBookmark)})
+        .update({'bookmark': FieldValue.arrayUnion(list)})
         .then((value) => print('Berhasil'))
         .catchError((error) => print('Gagal'));
   }
@@ -150,54 +81,17 @@ class FirestoreService {
     await akunCollection.doc(documentId).collection('bookmark').doc().update({'$index' : FieldValue.delete()});
   }
 
-  static Future<List<Webinar>> getBookmarkWebinar() async {
-    // Akun akun = await readDataBookmark();
-    String documentId = await AuthServices.getId();
-    var data = akunCollection.doc(documentId).collection('bookmark').doc().get() as List<Map<String,dynamic>>;
-    // var data = akun.bookmark as List<Map<String,dynamic>>;
-    List<Webinar> listWebinar = [];
-    if (data != null) {
-      for(var x in data){
-        listWebinar.add(Webinar(
-          judul: x['judul'],
-          penyelenggara: x['penyelenggara'],
-          deskripsi: x['deskripsi'],
-          tglAcara: x['tgl_acara'],
-          waktuAcara: x['waktu_acara'],
-          lokasiAcara: x['lokasi_acara'],
-          harga: x['harga'],
-          linkPendaftaran: x['link_pendaftaran'],
-          contactPerson: x['contact_person'],
-          sosmed: x['sosmed'],
-          benefit: x['benefit'],
-          urlPoster:x['url_poster'],
-          kategori: x['kategori'],
-        ));
+  static Future<bool> checkConnection() async {
+    try {
+      final connect =
+          await InternetAddress.lookup('google.com');
+      if (connect.isNotEmpty && connect[0].rawAddress.isNotEmpty) {
+        return true;
+      } else {
+        return false;
       }
+    } on SocketException catch (_) {
+      return false;
     }
-    return listWebinar;
-  }
-
-  static Future<List<Map<String, dynamic>>> convertToMap(
-      List<Webinar> listWebinar) async {
-    List<Map<String, dynamic>> listMap = [];
-    for (int x = 0; x < listWebinar.length; x++) {
-      listMap.add({
-        'judul': listWebinar[x].judul,
-        'penyelenggara': listWebinar[x].penyelenggara,
-        'deskripsi': listWebinar[x].deskripsi,
-        'tgl_acara': listWebinar[x].tglAcara,
-        'waktu_acara': listWebinar[x].waktuAcara,
-        'lokasi_acara': listWebinar[x].lokasiAcara,
-        'harga': listWebinar[x].harga,
-        'link_pendaftaran': listWebinar[x].linkPendaftaran,
-        'contact_person': listWebinar[x].contactPerson,
-        'sosmed': listWebinar[x].sosmed,
-        'benefit': listWebinar[x].benefit,
-        'url_poster': listWebinar[x].urlPoster,
-        'kategori': listWebinar[x].kategori,
-      });
-    }
-    return listMap;
   }
 }
--- lib/provider/akun_provider.dart
+++ lib/provider/akun_provider.dart
@@ -1,90 +1,125 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webinfo_senter/data/firebase/firestore_service.dart';
-import 'package:webinfo_senter/data/model/webinar.dart';
+import 'package:webinfo_senter/data/model/akun.dart';
+import 'package:webinfo_senter/helper/result_state.dart';
 
-enum ResultState {Loading, NoData, HasData, Error}
 
-class BookmarkProvider extends ChangeNotifier{
+class AkunProvider extends ChangeNotifier{
 
-  BookmarkProvider(){
-    _fetchDataBookmark();
-  }
-
-  List<Webinar> _bookmarkWebinar = [];
-  IconData _icon = Icons.bookmark_border;
+  late Akun _userAkun ;
   late ResultState _state ;
-  late ResultState _bookmarkState ;
-  
-  List<Webinar> get bookmarkWebinar => _bookmarkWebinar;
-  IconData get icon => _icon ;
+  String _message = '';
+
+  Akun get userAkun => _userAkun;
   ResultState get state => _state ;
-  ResultState get bookmarkState => _bookmarkState ;
+  String get message => _message ;
 
-  void addBookmark(Webinar webinar){
-    _bookmarkWebinar.add(webinar);
-    _icon = Icons.bookmarks ;
-    // getBookmarkByJudul(webinar.judul);
-    FirestoreService.addBookmark(bookmarkWebinar);
+  Future<void> fetchDataAkun() async {
+    _state = ResultState.loading;
     notifyListeners();
-  }
-
-  void deleteBookmark(Webinar webinar){
-    int? position ;
-    final result = bookmarkWebinar;
-    for(int x = 0 ; x < result.length ; x++){
-      if(result[x].judul == webinar.judul){
-        position = x ;
-      }
-    }
-    _bookmarkWebinar.remove(webinar);
-    _icon = Icons.bookmark_border ;
-    FirestoreService.deleteBookmark(position!);
-    notifyListeners();
-    //  getBookmarkByJudul(webinar.judul);
-  }
-
-  void getBookmarkByJudul(String judul) async {
     try{
-      final result = bookmarkWebinar;
-      bool isFound = false ;
-      for(var e in result){
-        if(e.judul == judul){
-          isFound = true ;
+      final getConnection =  await FirestoreService.checkConnection();
+      if(getConnection){
+        final Akun? snapshot = await FirestoreService.readDataAkun();
+        if(snapshot != null){
+          _userAkun = snapshot; 
+          _state = ResultState.hasData;
+          notifyListeners();
+        }else{
+          _state = ResultState.noData;
+          notifyListeners();
+          _message = 'Data not found';
         }
-      }
-      if(isFound){
-        _bookmarkState = ResultState.HasData;
-        _icon = Icons.bookmarks ;
-        notifyListeners();
       }else{
-        _bookmarkState = ResultState.NoData;
-        _icon = Icons.bookmark_border ;
+        _state = ResultState.error;
         notifyListeners();
+        _message = 'Check Your Internet Connection';
       }
-    }catch(e){ 
-      _bookmarkState = ResultState.Error;
-      notifyListeners();
-    }
+    }catch (e){
+      _state = ResultState.error;
+       notifyListeners();
+       _message = 'Error : $e';
+    } 
   }
+  
 
-  void _fetchDataBookmark() async {
-    _state = ResultState.Loading;
-    try{
-      final result = await FirestoreService.getBookmarkWebinar();
-      if(result.isNotEmpty){
-        _bookmarkWebinar = result ;
-        _state = ResultState.HasData;
-        notifyListeners();
-      }else{
-        _bookmarkWebinar.clear();
-        _state = ResultState.NoData;
-        notifyListeners();
-      }
-    }catch(e){
-      _state = ResultState.Error;
-      notifyListeners();
-    }
-  }
+
+
+  // List<Webinar> _bookmarkWebinar = [];
+  // IconData _icon = Icons.bookmark_border;
+  // late ResultState _state ;
+  // late ResultState _bookmarkState ;
+  
+  // List<Webinar> get bookmarkWebinar => _bookmarkWebinar;
+  // IconData get icon => _icon ;
+  // ResultState get state => _state ;
+  // ResultState get bookmarkState => _bookmarkState ;
+
+  // void addBookmark(Webinar webinar){
+  //   _bookmarkWebinar.add(webinar);
+  //   _icon = Icons.bookmarks ;
+  //   // getBookmarkByJudul(webinar.judul);
+  //   FirestoreService.addBookmark(bookmarkWebinar);
+  //   notifyListeners();
+  // }
+
+  // void deleteBookmark(Webinar webinar){
+  //   int? position ;
+  //   final result = bookmarkWebinar;
+  //   for(int x = 0 ; x < result.length ; x++){
+  //     if(result[x].judul == webinar.judul){
+  //       position = x ;
+  //     }
+  //   }
+  //   _bookmarkWebinar.remove(webinar);
+  //   _icon = Icons.bookmark_border ;
+  //   FirestoreService.deleteBookmark(position!);
+  //   notifyListeners();
+  //   //  getBookmarkByJudul(webinar.judul);
+  // }
+
+  // void getBookmarkByJudul(String judul) async {
+  //   try{
+  //     final result = bookmarkWebinar;
+  //     bool isFound = false ;
+  //     for(var e in result){
+  //       if(e.judul == judul){
+  //         isFound = true ;
+  //       }
+  //     }
+  //     if(isFound){
+  //       _bookmarkState = ResultState.HasData;
+  //       _icon = Icons.bookmarks ;
+  //       notifyListeners();
+  //     }else{
+  //       _bookmarkState = ResultState.NoData;
+  //       _icon = Icons.bookmark_border ;
+  //       notifyListeners();
+  //     }
+  //   }catch(e){ 
+  //     _bookmarkState = ResultState.Error;
+  //     notifyListeners();
+  //   }
+  // }
+
+  // void _fetchDataBookmark() async {
+  //   _state = ResultState.Loading;
+  //   try{
+  //     final result = await FirestoreService.getBookmarkWebinar();
+  //     if(result.isNotEmpty){
+  //       _bookmarkWebinar = result ;
+  //       _state = ResultState.HasData;
+  //       notifyListeners();
+  //     }else{
+  //       _bookmarkWebinar.clear();
+  //       _state = ResultState.NoData;
+  //       notifyListeners();
+  //     }
+  //   }catch(e){
+  //     _state = ResultState.Error;
+  //     notifyListeners();
+  //   }
+  // }
 
 }
