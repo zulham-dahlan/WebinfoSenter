2754bfc15e489ec3b1fd6a202011711b7d97fcf5
zulham321 <zulhamlanz@gmail.com>
2022-01-11 02:46:53 +0700

    Merubah tampilan login

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,11 +1,10 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webinfo_senter/common/text_theme.dart';
 import 'package:webinfo_senter/data/firebase/auth_service.dart';
 import 'package:webinfo_senter/common/style.dart';
 import 'package:webinfo_senter/ui/layout_navigation.dart';
 import 'package:webinfo_senter/ui/register_page.dart';
-
 
 class LoginPage extends StatefulWidget {
   static const routeName = '/login_page';
@@ -18,18 +17,17 @@ class _LoginPage extends State<LoginPage> {
   bool _isObscure = true;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  
+
   @override
   Widget build(BuildContext context) {
     double widthButton, heightButton;
 
-    if (MediaQuery.of(context).orientation == Orientation.landscape){
+    if (MediaQuery.of(context).orientation == Orientation.landscape) {
       widthButton = MediaQuery.of(context).size.height * 0.933;
       heightButton = MediaQuery.of(context).size.height * 0.145;
-    }
-    else{
+    } else {
       widthButton = MediaQuery.of(context).size.width * 0.933;
-      heightButton = MediaQuery.of(context).size.width *0.145;
+      heightButton = MediaQuery.of(context).size.width * 0.145;
     }
 
     return Scaffold(
@@ -37,111 +35,208 @@ class _LoginPage extends State<LoginPage> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  child: Container(child: Image.asset('assets/woman-working-at-home.png'),padding: const EdgeInsets.all(20),),
-                  margin: const EdgeInsets.only(
-                      top: 12, right: 12, left: 12, bottom: 21),
-                ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(left:12,bottom: 9),
-                  child: Text('Email',style: stylePoppins.copyWith(fontWeight: FontWeight.bold, color: customGreyLight,)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 12,right: 12,bottom: 21),
-                  child: TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'username@gmail.com',labelStyle: stylePoppins.copyWith(fontWeight: FontWeight.bold, color: customGreyLight,),
-                    ),
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/logo.png',
+                    width: 180,
+                    height: 180,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(left:12,bottom: 10),
-                  child: Text('Password',style: stylePoppins.copyWith(fontWeight: FontWeight.bold, color: customGreyLight,)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 12,right: 12,bottom: 18),
-                  child: TextField(
-                    controller: _passwordController,
-                    obscureText: _isObscure,
-                    decoration: InputDecoration(
-                        hintText: 'Password',labelStyle: stylePoppins.copyWith(fontWeight: FontWeight.bold, color: customGreyLight,),
-                        border: OutlineInputBorder(),
-                        suffixIcon: IconButton(
-                            icon: Icon(
-                                _isObscure ? Icons.visibility : Icons.visibility_off),
-                            onPressed: () {
-                              setState(() {
-                                _isObscure = !_isObscure;
-                              });
-                            })),
+                Text(
+                  'Login',
+                  style: myTextTheme.headline4!.copyWith(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Login to get webinar information',
+                  style: myTextTheme.bodyText1,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'username@gmail.com',
+                    labelText: 'Email',
+                    labelStyle: myTextTheme.bodyText1,
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    labelStyle: myTextTheme.bodyText1,
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        icon: Icon(_isObscure
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        }),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+
                 Container(
-                  alignment: Alignment.centerRight,
-                  margin: EdgeInsets.only(right: 12),
-                  child:           TextButton(
+                  width: widthButton,
+                  height: heightButton,
+                  child: ElevatedButton(
                     onPressed: () {
-                      Navigator.pushReplacementNamed(context, LayoutNavigation.routeName);
+                      AuthServices.lognIn(_emailController.text,
+                          _passwordController.text, context);
                     },
-                    child: Text('Lupa Password Nih?',style: styleRoboto.copyWith(fontSize: 12, color: customBlueDark,)),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 12, left: 12, top:9, bottom: 9),
-                  child: Container(
-                    width: widthButton,
-                    height: heightButton,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        AuthServices.lognIn(_emailController.text, _passwordController.text, context);
-                      },
-                      child: Text("Masuk",style: stylePoppins.copyWith(color: Colors.white,),),
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),)),
-                        backgroundColor:
-                        MaterialStateProperty.all(customRedColor),
-                        padding: MaterialStateProperty.all(EdgeInsets.only(
-                            right: 153, left: 153, top: 22, bottom: 22)),
-                      ),
+                    child: Text(
+                      "Login",
+                      style: myTextTheme.button,
+                    ),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      )),
+                      backgroundColor:
+                          MaterialStateProperty.all(customRedColor),
                     ),
                   ),
+                ),
+                SizedBox(
+                  height: 10,
                 ),
                 Container(
                   alignment: Alignment.center,
-                  margin: EdgeInsets.only(left:12,bottom: 9),
-                  child: Text('Atau',style: stylePoppins.copyWith(fontWeight: FontWeight.bold, color: customGreyLight,)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 12, left: 12, bottom: 9),
-                  child: Container(
-                    width: widthButton,
-                    height: heightButton,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, RegisterPage.routeName);
-                      },
-                      child: Text("Kuy Gabung",style: stylePoppins),
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                side: BorderSide(color: customGreyLight))),
-                        backgroundColor:
-                        MaterialStateProperty.all(Colors.white),
-                        padding: MaterialStateProperty.all(EdgeInsets.only(
-                            right: 135, left: 135, top: 22, bottom: 22)),
-                      ),
-                    ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(
+                          context, LayoutNavigation.routeName);
+                    },
+                    child: Text('Lupa password ?',
+                        style: myTextTheme.bodyText1!.copyWith(
+                          color: customBlueDark,
+                        )),
                   ),
                 ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('Belum Punya Akun?',
+                        style: myTextTheme.bodyText1!
+                            .copyWith(color: Colors.grey)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: widthButton,
+                      height: heightButton,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, RegisterPage.routeName);
+                        },
+                        child: Text('Daftar Webinfo Senter',
+                            style: myTextTheme.button!
+                                .copyWith(color: Colors.black)),
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      side:
+                                          BorderSide(color: customGreyLight))),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+                // Container(
+                //   margin:
+                //       EdgeInsets.only(right: 12, left: 12, top: 9, bottom: 9),
+                //   child: Container(
+                //     width: widthButton,
+                //     height: heightButton,
+                //     child: ElevatedButton(
+                //       onPressed: () {
+                //         AuthServices.lognIn(_emailController.text,
+                //             _passwordController.text, context);
+                //       },
+                //       child: Text(
+                //         "Masuk",
+                //         style: stylePoppins.copyWith(
+                //           color: Colors.white,
+                //         ),
+                //       ),
+                //       style: ButtonStyle(
+                //         shape:
+                //             MaterialStateProperty.all<RoundedRectangleBorder>(
+                //                 RoundedRectangleBorder(
+                //           borderRadius: BorderRadius.circular(10.0),
+                //         )),
+                //         backgroundColor:
+                //             MaterialStateProperty.all(customRedColor),
+                //         padding: MaterialStateProperty.all(EdgeInsets.only(
+                //             right: 153, left: 153, top: 22, bottom: 22)),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                // Container(
+                //   alignment: Alignment.center,
+                //   margin: EdgeInsets.only(left: 12, bottom: 9),
+                //   child: Text('Atau',
+                //       style: stylePoppins.copyWith(
+                //         fontWeight: FontWeight.bold,
+                //         color: customGreyLight,
+                //       )),
+                // ),
+                // Container(
+                //   margin: EdgeInsets.only(right: 12, left: 12, bottom: 9),
+                //   child: Container(
+                //     width: widthButton,
+                //     height: heightButton,
+                //     child: ElevatedButton(
+                //       onPressed: () {
+                //         Navigator.pushNamed(context, RegisterPage.routeName);
+                //       },
+                //       child: Text("Kuy Gabung", style: stylePoppins),
+                //       style: ButtonStyle(
+                //         shape:
+                //             MaterialStateProperty.all<RoundedRectangleBorder>(
+                //                 RoundedRectangleBorder(
+                //                     borderRadius: BorderRadius.circular(10.0),
+                //                     side: BorderSide(color: customGreyLight))),
+                //         backgroundColor:
+                //             MaterialStateProperty.all(Colors.white),
+                //         padding: MaterialStateProperty.all(EdgeInsets.only(
+                //             right: 135, left: 135, top: 22, bottom: 22)),
+                //       ),
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
@@ -149,6 +244,11 @@ class _LoginPage extends State<LoginPage> {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 }
-
-
